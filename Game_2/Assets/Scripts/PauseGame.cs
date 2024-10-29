55fafd92a93bb1e0fdbcbc8f66d626db21813646
Francesco Spagnolo <francescospag24@gmail.com>
2024-10-28 21:07:40 -0400

    UI/Pause menu and a new main scene
    Includes TMP and other assets plus scripts to make the pause menu basics

---
+++ Game_2/Assets/Scripts/PauseGame.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseGame : MonoBehaviour
+{
+    public GameObject PauseMenuUI;
+    // public GameObject[] player;
+
+    public void Pause()
+    {
+        // if not paused
+        if (Time.timeScale == 1)
+        {
+            // pause and activate pause menu
+            Time.timeScale = 0;
+            PauseMenuUI.SetActive(true);
+        }
+        else
+        {
+            // if paused, resume game and remove pause menu
+            Time.timeScale = 1;
+            PauseMenuUI.SetActive(false);
+        }
+    }
+
+    public void Restart()
+    {
+        // Reset every player and game element to base values
+        // including time, position, score, health and energy, inventory, etc.
+        Time.timeScale = 1;
+        PauseMenuUI.SetActive(false);
+        SceneManager.LoadScene("Main");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quitting Game...");
+        Application.Quit();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+        }
+    }
+}
