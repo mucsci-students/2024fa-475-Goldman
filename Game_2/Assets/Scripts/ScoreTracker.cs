7c5f9a601113d7a3ec0ca059c20a99a69c36e5ee
Sam8718 <samanthams49@comcast.net>
2024-11-01 08:16:11 -0400

    commiting before class
    just making sure I have my changes for class

---
+++ Game_2/Assets/Scripts/ScoreTracker.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ScoreTracker : MonoBehaviour {
+/*
+    int highestTile = 2;
+    int score = 0;
+*/
+//public class MainScore(){
+    /*if (tiles merged){
+        score += new tile number;
+    }
+    */
+//}
+
+//public class HighestTile(){
+    /* if(new tile > highestTile)
+           highestTile = new tile;
+    */
+//}
+
+
+}
