822b093949d727a2859fe35e38994dc2dcd06e6b
Francesco Spagnolo <francescospag24@gmail.com>
2024-11-03 12:49:04 -0500

    Title Screen Additions

--- Game_2/Assets/Scripts/MainMenuUI.cs
+++ Game_2/Assets/Scripts/MainMenuUI.cs
@@ -5,9 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuUI : MonoBehaviour
 {
-    public void Start()
+    public GameObject MainMenu;
+    public GameObject GameModesUI;
+    public GameObject OptionsMenuUI;
+    public GameObject HelpText;
+    public void StartGame()
     {
         SceneManager.LoadScene("Main");
+    }
+
+    public void Start()
+    {
+        MainMenu.SetActive(true);
+        GameModesUI.SetActive(false);
+        OptionsMenuUI.SetActive(false);
+    }
+    public void GameModes()
+    {
+        Debug.Log("Going to Game Modes");
+        MainMenu.SetActive(false);
+        GameModesUI.SetActive(true);
+    }
+
+    public void Options()
+    {
+        Debug.Log("Going to options");
+        MainMenu.SetActive(false);
+        OptionsMenuUI.SetActive(true);
     }
 
     public void Quit()
@@ -15,9 +39,25 @@ public class MainMenuUI : MonoBehaviour
         Debug.Log("Quitting Game...");
         Application.Quit();
     }
-
-    void Update()
+    public void Help()
     {
+        Debug.Log("Displaying Help");
+        HelpText.SetActive(true);
+    }
 
+    public void GameMenuBack()
+    {
+        GameModesUI.SetActive(false);
+        MainMenu.SetActive(true);
+    }
+    public void OptionsMenuBack()
+    {
+        OptionsMenuUI.SetActive(false);
+        MainMenu.SetActive(true);
+    }
+
+    public void HelpTextBack()
+    {
+        HelpText.SetActive(false);
     }
 }
