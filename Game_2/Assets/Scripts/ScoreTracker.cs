79907341396dd2e5f3f5219753e06e3962f4549b
Sam8718 <samanthams49@comcast.net>
2024-11-03 08:49:35 -0500

    Score tracker script
    attempted to rework the score script.

--- Game_2/Assets/Scripts/ScoreTracker.cs
+++ Game_2/Assets/Scripts/ScoreTracker.cs
@@ -3,23 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class ScoreTracker : MonoBehaviour {
-/*
+public class ScoreTracker : MonoBehaviour { /*
     int highestTile = 2;
     int score = 0;
-*/
-//public class MainScore(){
-    /*if (tiles merged){
+    Rigidbody2D tile1;   //make whatever the tiles are listed as when seeing if
+    Rigidbody2D tile2;   //two tiles can/have merged together to look at scores
+    
+
+//may have to be the built in update method
+public void MainScore(){
+    if (!notYetMerged){   //using value from TileScript script
         score += new tile number;
+    } else {
+        notYetMerged = true;
     }
-    */
-//}
+}
 
-//public class HighestTile(){
-    /* if(new tile > highestTile)
-           highestTile = new tile;
-    */
-//}
-
-
+//also may have to be in the update method
+public void HighestTile(Rigidbody2D newTile){
+    if(newTile > highestTile)
+        highestTile = newTile.value;  
+}
+*/
 }
