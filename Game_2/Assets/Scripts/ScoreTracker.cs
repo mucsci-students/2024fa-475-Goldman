2ae21465905f42577351fd354b176c74f81e8540
Sam8718 <samanthams49@comcast.net>
2024-11-03 13:52:07 -0500

    Reworked the score again
    Still commented out, have to figure out how to get tile to read from TileScript

--- Game_2/Assets/Scripts/ScoreTracker.cs
+++ Game_2/Assets/Scripts/ScoreTracker.cs
@@ -6,23 +6,25 @@ using UnityEngine;
 public class ScoreTracker : MonoBehaviour { /*
     int highestTile = 2;
     int score = 0;
-    Rigidbody2D tile1;   //make whatever the tiles are listed as when seeing if
-    Rigidbody2D tile2;   //two tiles can/have merged together to look at scores
+
+void Start(){
+    TileScript tile = GetComponent<TileScript>();
+}
     
 
 //may have to be the built in update method
 public void MainScore(){
-    if (!notYetMerged){   //using value from TileScript script
-        score += new tile number;
+    if (!tile.notYetMerged){   //using value from TileScript script
+        score += tile.value;
     } else {
-        notYetMerged = true;
+        tile.notYetMerged = true;
     }
 }
 
 //also may have to be in the update method
-public void HighestTile(Rigidbody2D newTile){
-    if(newTile > highestTile)
-        highestTile = newTile.value;  
+public void HighestTile(Rigidbody2D other){
+    if(other.tile.value > tile.value)
+        highestTile = other.tile.value;  
 }
 */
 }
