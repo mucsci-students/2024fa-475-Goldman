9a0a196815569966fd02afa997be7b7ef54c4ce3
john-hershey <jjhersh1@millersville.edu>
2024-11-12 16:38:38 -0500

    new level added

--- Game_2/Assets/Scripts/ScoreTracker.cs
+++ Game_2/Assets/Scripts/ScoreTracker.cs
@@ -7,14 +7,18 @@ public class ScoreTracker : MonoBehaviour {
     //public TileManager script;
     private int highestTile = 4;
     private int score = 0;
+
     public Text scoreText;
     public Text highText;
+    public string scoreSaveText;
+    public string tileSaveText;
+
 
     void Start()
     {
         //score = PlayerPrefs.GetInt("MainTile", 0);
         
-        highestTile = PlayerPrefs.GetInt("MainTile", 4);
+        highestTile = PlayerPrefs.GetInt(tileSaveText, 4);
         highText.text = "Best Tile: " + highestTile;
     }
     public void MainScore(int tileVal){
@@ -26,12 +30,22 @@ public class ScoreTracker : MonoBehaviour {
         scoreText.text = "Score: " + score;
         //Debug.Log(score);
     }
+    //scoring for 2v2
+    public void VersusScore()
+    {
+
+    }
+    //scoring for 8x8
+    public void BigScore()
+    {
+
+    }
     public void SaveScore(){
-        if(PlayerPrefs.GetInt("MainScore", 0) < score){
-            PlayerPrefs.SetInt("Mainscore", score);
+        if(PlayerPrefs.GetInt(scoreSaveText, 0) < score){
+            PlayerPrefs.SetInt(scoreSaveText, score);
         }
-        if(PlayerPrefs.GetInt("MainTile", 4) < highestTile){
-            PlayerPrefs.SetInt("MainTile", highestTile);
+        if(PlayerPrefs.GetInt(tileSaveText, 4) < highestTile){
+            PlayerPrefs.SetInt(tileSaveText, highestTile);
         }
     }
 }
