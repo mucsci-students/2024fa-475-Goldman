f5af7ec5d2275132a104f2ef12abd0d6ff75b12e
Sam8718 <samanthams49@comcast.net>
2024-11-13 16:51:07 -0500

    script
    small change

--- Game_2/Assets/Scripts/StoryScript.cs
+++ Game_2/Assets/Scripts/StoryScript.cs
@@ -4,34 +4,38 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class StoryScript : MonoBehaviour {
-    public string scoreSaveText;
-    private int highestTile;
+
+    private int highestTile = 2;
     public Text storyText;
+   // public string scoreSaveText;
+    public string tileSaveText;
 
     void Start() {
-        highestTile = PlayerPrefs.GetInt(scoreSaveText, 2);
-        Story(highestTile);
+        highestTile = PlayerPrefs.GetInt(tileSaveText, 2);
+        storyText.text = Story(highestTile);
     }
 
-    public void Story(int highestTile) {
+    public string Story(int highestTile) {
         if(highestTile == 2 || highestTile == 4) {
-            storyText.text = "2 & 4";
+            return storyText.text = "2 & 4";
         } else if(highestTile == 8 || highestTile == 16) {
-            storyText.text = "8 & 16";
+            return storyText.text = "8 & 16";
         } else if(highestTile == 32 || highestTile == 64) {
-            storyText.text = "32 & 64";
+            return storyText.text = "32 & 64";
         } else if(highestTile == 128 || highestTile == 256){
-            storyText.text = "128 & 256";
+            return storyText.text = "128 & 256";
         } else if(highestTile == 512){
-            storyText.text = "512";
+            return storyText.text = "512";
         } else if(highestTile == 1024){
-            storyText.text = "1024";
+            return storyText.text = "1024";
         } else if(highestTile == 2048){
-            storyText.text = "2048";
+            return storyText.text = "2048";
         } else if(highestTile == 4096){
-            storyText.text = "4096";
+            return storyText.text = "4096";
         } else if(highestTile == 8192){
-            storyText.text = "8192";
+            return storyText.text = "8192";
+        } else {
+            return "";
         }
     }
 }
